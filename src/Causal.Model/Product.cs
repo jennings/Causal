275110b80f810c12a75491fc0601e17f662a222a
Stephen Jennings <stephen.g.jennings@gmail.com>
2014-05-14 22:07:11 -0700

    Add JsonSerializedConfiguration to store products between launches

---
+++ src/Causal.Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Causal.Model
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+    }
+}
---
+++ src/Causal.Updater/Storage/IConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Causal.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Causal.Updater.Storage
+{
+    interface IConfiguration
+    {
+        ICollection<Product> Products { get; }
+        void DiscardChanges();
+        void SaveChanges();
+    }
+}
